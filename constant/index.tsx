8627728569d6dd1045f00151cea024db254d53fe
Mengsroin <hengmengsroin@gmail.com>
2024-01-09 14:01:25 +0700

    Update banner name and social media links

--- constant/index.tsx
+++ constant/index.tsx
@@ -1,21 +1,21 @@
 export const Social_Icons = [
   {
-    link: "https://github.com/ibrahimmemonn",
+    link: "https://github.com/hengmengsroin",
     image: "/Github.svg",
     alt: "Github",
   },
   {
-    link: "https://www.instagram.com/ibii.memon/?hl=en",
+    link: "https://www.instagram.com/hengmengsroin",
     image: "/Instagram.svg",
     alt: "Instagram",
   },
   {
-    link: "https://www.linkedin.com/in/ibrahimmemonn/",
+    link: "https://www.linkedin.com/in/hengmengsroin",
     image: "/LinkedIn.svg",
     alt: "LinkedIn",
   },
   {
-    link: "https://twitter.com/Ibrahimmemonnn",
+    link: "https://twitter.com/hengmengsroin",
     image: "/Twitter.svg",
     alt: "Twitter",
   },
